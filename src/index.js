dbd47a2a1771a05ecfe238ab8e7bd8dfc5a4103c
Serhiy Korytskyy <korysergey55@gmail.com>
2021-05-18 21:46:58 +0300

    mvp-done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+import debounce from 'lodash.debounce';
+import { alert, error, defaults } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+import {fetchData, endPromise} from './js/api';
+import list from './tpl/list.hbs';
+import main from './tpl/main.hbs';
+
+const listRef = document.querySelector('.country-list');
+const inputRef = document.querySelector('.input');
+
+inputRef.addEventListener('input', debounce(inputChange, 500));
+
+function inputChange(event) {
+  const inputRef = event.target.value;
+  const inputValue = inputRef.toUpperCase().trim();
+  listRef.innerHTML = '';
+  if (!inputValue) return;
+  endPromise(inputRef)
+    .then(user => userCountries(user))
+    .catch(user => userCountriesError(error));
+}
+
+const userCountriesError = error => {
+  alert({ text: error });
+};
+
+const userCountries = country => {
+    if(country.length >= 2 && country.length <= 10){
+        const countryElems = list(country);
+        listRef.innerHTML = countryElems;
+    }
+    if(country.length === 1 ){
+        const countryElems = main(country);
+        listRef.innerHTML = countryElems;
+    }
+    if(country.length > 10){
+      alert ({text: 'error'});
+    }
+};
---
+++ src/js/api.js
@@ -0,0 +1,15 @@
+const JSON_BASE_URL = 'https://restcountries.eu/rest/v2';
+
+const fetchData = (request = '/') => {
+  fetch(JSON_BASE_URL + request).then(response => response.json());
+};
+
+const endPromise = name =>
+  fetch(`${JSON_BASE_URL}/name/${name}`).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise(`error ${name}`);
+  });
+  
+ export {fetchData, endPromise};
